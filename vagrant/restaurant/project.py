c1fa3d0e194bd2f2b0c1601198f31fbf9079b1fb
Lena <lzhengem@gmail.com>
2019-01-16 20:38:07 -0800

    feat: added project.py to try out flask framework

---
+++ vagrant/restaurant/project.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__) #create instance of flask, using the current name of running application as argument
+
+
+@app.route('/') #decorator in python. this means that whenever we get a hit on / or /hello, the HelloWorld() function gets invoked
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World" #writes out hello world to the browser
+
+if __name__ == '__main__': #application run by the Python interpreter gets a name 
+# variable set to __main__, whereas all other imported python files get a __name__ set to the actual name of the python file
+                            #this makes sure that this code only gets run if it is run from the python interpreter, not as an imported module 
+    app.debug = True    #this allows you not to have to restart your server everytime you edit soemthing
+    app.run(host='0.0.0.0', port=5000) #makes vagrant to listen to all public ip addresses
