1cd231ffdbd7cbbf545bbf31261ef13c86c7d7b7
Lena <lzhengem@gmail.com>
2019-01-19 17:03:16 -0800

    feat: able to handle post requests on newMenuItem. It will add the new menuitem to the restaurant and then redirect to restaurantMenu

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,6 +19,13 @@ def restaurantMenu(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])    
 def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+
+
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
