afb01320c669f0ec37dec85c56081d6029c5da10
Lena <lzhengem@gmail.com>
2019-01-19 22:53:43 -0800

    feat: added all the routes for the 8 webpages.
    showRestaurants, newRestaurant, editRestaurant, showMenu, newMenuItem, editMenuItem, deleteMenuItem

--- vagrant/restaurant/finalproject.py
+++ vagrant/restaurant/finalproject.py
@@ -14,6 +14,32 @@ session = DBSession()
 def showRestaurants():
     return "This page will show all my restaurants"
 
+@app.route("/restaurant/new/")
+def newRestaurant():
+    return "This page will be for making a new restaurant"
+
+@app.route("/restaurant/<int:restaurant_id>/edit/")
+def editRestaurant(restaurant_id):
+    return "This page will be for editing restaurant %s" % restaurant_id
+
+@app.route("/restaurant/<int:restaurant_id>/")
+@app.route("/restaurant/<int:restaurant_id>/menu/")
+def showMenu(restaurant_id):
+    return "This page is the meny for restaurant %s" % restaurant_id
+
+@app.route("/restaurant/<int:restaurant_id>/new/")
+def newMenuItem(restaurant_id):
+    return "This page will be for making a new menu item for restaurant %s" % restaurant_id
+
+@app.route("/restaurant/<int:restaurant_id>/<int:menu_id>/edit/")
+def editMenuItem(restaurant_id,menu_id):
+    return "This page will be for editing menu item %s" % menu_id
+
+@app.route("/restaurant/<int:restaurant_id>/<int:menu_id>/delete/")
+def deleteMenuItem(restaurant_id,menu_id):
+    return "This page is for deleting menu item %s" % menu_id
+
+
 
 if __name__ == '__main__':
     app.debug = True
