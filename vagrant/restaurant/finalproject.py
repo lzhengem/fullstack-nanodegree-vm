9a597d1e57c98699afc6fd78e1dcad654dd2347f
Lena <lzhengem@gmail.com>
2019-01-19 22:43:34 -0800

    feat: added show restaurants placeholder

--- vagrant/restaurant/finalproject.py
+++ vagrant/restaurant/finalproject.py
@@ -1,6 +1,20 @@
-from flask import flask
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
-if name == '__main__':
+engine = create_engine('sqlite:///restaurantmenu.db', connect_args={'check_same_thread':False})
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route("/")
+@app.route("/restaurants/")
+def showRestaurants():
+    return "This page will show all my restaurants"
+
+
+if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
