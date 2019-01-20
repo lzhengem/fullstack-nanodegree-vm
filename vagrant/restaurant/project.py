7afd1e240fb336a10406beab050212c35eb8ac67
Lena <lzhengem@gmail.com>
2019-01-19 18:48:53 -0800

    feat: created API endpoint restaurantMenuJSON
    when people go to /restaurants/<int:restaurant_id>/JSON, it returns a JSON object instead of a page

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -9,6 +9,13 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+#make API endpoint (GET request)
+@app.route('/restaurants/<int:restaurant_id>/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    return jsonify(MenuItems=[i.serialize for i in items])
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
