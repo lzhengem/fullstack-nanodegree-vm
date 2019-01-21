2a696fd251b3baced4b23eef890e454e8c5238dc
Lena <lzhengem@gmail.com>
2019-01-20 22:39:43 -0800

    feat: added JSON endpoints for showRestaurantsJSON, showMenuJSON, showMenuItemJSON

--- vagrant/restaurant/finalproject.py
+++ vagrant/restaurant/finalproject.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -108,10 +108,26 @@ def deleteMenuItem(restaurant_id,menu_id):
     else:
         return render_template('deleteMenuItem.html',restaurant=restaurant,item=item)
 
-    
+#create api endpoint for /restaurants/JSON
+@app.route('/restaurants/JSON/')
+def showRestaurantsJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(Restaurants = [restaurant.serialize for restaurant in restaurants])
 
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def showMenuJSON(restaurant_id):
+    # restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems = [item.serialize for item in items])
+    
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def showMenuItemJSON(restaurant_id,menu_id):
+    # restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem = item.serialize)
 
 
 if __name__ == '__main__':
+    app.secret_key = "super_secret_key"
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
