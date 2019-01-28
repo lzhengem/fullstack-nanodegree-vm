3563f9867c5429bff0aca1727b5cc75cff621171
Lena <lzhengem@gmail.com>
2019-01-27 20:04:47 -0800

    feat: updated database_setup.py to include User class
    User has id, name, email, picture. Restaurant and Menu Item will have new column user_id and a new relationship user that links to a specific user. the new database that will get created will be called restaurantmenuwithusers.db so it doesn't overwrite the old database

--- vagrant/OAuth2.0/database_setup.py
+++ vagrant/OAuth2.0/database_setup.py
@@ -5,11 +5,21 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250),nullable=False)
+    picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -17,6 +27,7 @@ class Restaurant(Base):
        return {
            'name'         : self.name,
            'id'           : self.id,
+           'user_id'      : self.user_id
        }
  
 class MenuItem(Base):
@@ -31,6 +42,9 @@ class MenuItem(Base):
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 
     @property
     def serialize(self):
@@ -41,11 +55,12 @@ class MenuItem(Base):
            'id'         : self.id,
            'price'         : self.price,
            'course'         : self.course,
+           'user_id'      : self.user_id
        }
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
