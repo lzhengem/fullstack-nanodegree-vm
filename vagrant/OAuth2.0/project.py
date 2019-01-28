66db5df7c854d9647c321c999c01fb271ce69b4b
Lena <lzhengem@gmail.com>
2019-01-27 20:26:08 -0800

    feat: added 3 methods: createUser, getUserInfo, getUserID
    createUser creates a new user using the info stored in the login_session and saves it in the database. getUserInfo returns a user based on their id. getUserID returns a user id based on the user's email

--- vagrant/OAuth2.0/project.py
+++ vagrant/OAuth2.0/project.py
@@ -293,9 +293,28 @@ def deleteMenuItem(restaurant_id,menu_id):
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('deleteMenuItem.html', item = itemToDelete)
+        
+#get a user id based on the user's email        
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
 
 
+#get a user based on their id
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
 
+#creates a new user using the info stored in the login_session and saves it in the database
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email = login_session['email'], picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user.id
 
 if __name__ == '__main__':
   app.secret_key = 'super_secret_key'
