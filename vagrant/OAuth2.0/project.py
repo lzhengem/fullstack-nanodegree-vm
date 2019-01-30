b66b59903944304fec132214bd42343d97c1d338
Lena <lzhengem@gmail.com>
2019-01-29 19:54:15 -0800

    feat: added fbconnect method in project.py so user can log in to our website via facebook.

--- vagrant/OAuth2.0/project.py
+++ vagrant/OAuth2.0/project.py
@@ -158,6 +158,62 @@ def gdisconnect():
         response.headers['Content-type'] = 'application/json'
         return response
 
+@app.route('/fbconnect',methods=["POST"])
+def fbconnect():
+    #check to see if the state matches
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'),401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    access_token = request.data
+
+    #Exchange client token for long-lived server-side token with GET /oauth/access_token?grant_type=fb_exchange_token&client_id=[app-id]&client_secret={client_secret}&fb_exchange_token={short-lived-token}
+    app_id = json.loads(open('fb_client_secrets.json','r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json','r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s'% (app_id,app_secret,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    #use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    #strip expire tag from access token
+    token = result.split("&")[0]
+
+    url = 'https://graph.facebook.com/v2.2/me?%s' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    #print "url" sent for APIR access:%s" %url
+    #print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data['email']
+    login_session['facebook_id'] = data['id']
+
+    #get user picture
+    url = 'https://graph.facebook.com/v2.2/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+    login_session['picture'] = data['data']['url']
+
+    #see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+        login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome,'
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
