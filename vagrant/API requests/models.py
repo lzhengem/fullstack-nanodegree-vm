2c2098b1fc5b310a2a04f32fe38d72f9de1a9b03
Lena <lzhengem@gmail.com>
2019-02-09 23:35:19 -0800

    feat: updated models.py with udacity's new starter code 06_Adding Features to your Mashup

--- vagrant/API requests/models.py
+++ vagrant/API requests/models.py
@@ -1,27 +1,34 @@
-from sqlalchemy import Column, Integer, String
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
+
+
+
+
 Base = declarative_base()
+class Restaurant(Base):
+  __tablename__ = 'restaurant'
+  id = Column(Integer, primary_key = True)
+  restaurant_name = Column(String)
+  restaurant_address = Column(String)
+  restaurant_image = Column(String)
+  
+  
+  #Add a property decorator to serialize information from this database
+  @property
+  def serialize(self):
+    return {
+      'restaurant_name': self.restaurant_name,
+      'restaurant_address': self.restaurant_address,
+      'restaurant_image' : self.restaurant_image,
+      'id' : self.id
+      
+      }
 
-class Puppy(Base):
-    __tablename__ = 'puppy'
-
-
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(String(250))
-
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-            'id': self.id,
-           'name': self.name,
-           'description' : self.description
-       }
+engine = create_engine('sqlite:///restaurants.db')
  
 
-
-engine = create_engine('sqlite:///puppies.db')
 Base.metadata.create_all(engine)
