ff49e8bec1b1b90225f04fdfa5dfe33e85fe6b8c
Lena <lzhengem@gmail.com>
2019-02-11 21:06:33 -0800

    feat: all_restaurants_handler shows all the restaurants in database if there is a get request to /restaurants. and it creates a new restaurant if it is a post request with location and mealtype

--- vagrant/API requests/views.py
+++ vagrant/API requests/views.py
@@ -19,7 +19,7 @@ sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 #google_api_key = ''
 
-engine = create_engine('sqlite:///restaurants.db')
+engine = create_engine('sqlite:///restaurants.db',connect_args={'check_same_thread':False})
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -29,10 +29,33 @@ app = Flask(__name__)
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
   #YOUR CODE HERE
+    if request.method == 'GET':
+        restaurant = session.query(Restaurant).all()
+        return jsonify(restaurants = [i.serialize for i in restaurant])
+
+    elif request.method == 'POST':
+        location = request.args.get('location','')
+        mealtype = request.args.get('mealtype','')
+        restaurant_info = findARestaurant(request.args.get('mealtype'), request.args.get('location'))
+        if restaurant_info != "No Restaurants Found":
+            # return jsonify(restaurant_info)
+            restaurant = Restaurant(restaurant_name= restaurant_info["restaurant_name"], restaurant_address= restaurant_info["restaurant_address"], restaurant_image = restaurant_info["restaurant_image"] )
+            session.add(restaurant)
+            session.commit()
+            return jsonify(restaurant.serialize)
+            # all_restaurant = session.query(Restaurant).all()
+            # print 'all:' 
+            # print all_restaurant
+        else:
+            return jsonify({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
+
+    
     
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
   #YOUR CODE HERE
+  if request.method == 'GET':
+    return 'hi'
 
 if __name__ == '__main__':
     app.debug = True
