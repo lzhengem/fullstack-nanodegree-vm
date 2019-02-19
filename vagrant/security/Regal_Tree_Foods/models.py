93ce8f8c7a4fa1468789ca44dc86ba27111ebc54
Lena <lzhengem@gmail.com>
2019-02-18 17:37:59 -0800

    feat: added new fields to user: picture and email

--- vagrant/security/Regal_Tree_Foods/models.py
+++ vagrant/security/Regal_Tree_Foods/models.py
@@ -14,7 +14,9 @@ secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(32), index=True)
+    username = Column(String(32))
+    picture = Column(String)
+    email = Column(String, index=True)
     password_hash = Column(String(64))
 
     def hash_password(self, password):
