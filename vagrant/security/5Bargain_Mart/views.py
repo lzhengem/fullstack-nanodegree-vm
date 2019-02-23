736bf415227e6b2e27aabefaee400ba6ed92d493
Lena <lzhengem@gmail.com>
2019-02-22 16:17:00 -0800

    feat: added RateLimit class to views that contains the limit and reset time

--- vagrant/security/5Bargain_Mart/views.py
+++ vagrant/security/5Bargain_Mart/views.py
@@ -20,6 +20,24 @@ session = DBSession()
 app = Flask(__name__)
 
 #ADD RATE LIMITING CODE HERE
+class RateLimit(object):
+    expiration_window = 10
+    def __init__(self, key_prefix,limit,per,send_x_headers):
+        self.reset = (int(time.time())//per) * per + per
+        self.key = key_prefix + self.reset
+        self.limit = limit
+        self.per = per
+        self.send_x_headers = send_x_headers
+
+        p = redis.pipeline()
+        p.incr(self.key)
+        p.expireat(self.key,self.reset + expiration_window)
+        self.current = min(p.execute()[0],limit)
+
+    remaining = property(lambda x: x.limit - x.current)
+    over_limit = property(lambda x: x.current >= x.limit)
+
+
 
 
 @ratelimit(limit=60,per=60) #only allow 60 requests per 60 seconds
